bc8bfad5b9b5df4edfa78afd749cff4797e90c7c
Patrikator17 <patkarpratik42>
2024-06-18 21:47:02 +0530

    Add Next.js project files

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,6 @@
+const SinglePage = () => {
+    return(
+        <div className="Single Page">Single Page</div>
+    )
+}
+export default SinglePage
---
+++ src/app/list/page.tsx
@@ -0,0 +1,7 @@
+const ListPage = () => {
+    return(
+        <div className="ListPage">List Page</div>
+    )
+}
+
+export default ListPage
