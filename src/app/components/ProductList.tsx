8eb35fd16e6501caf6272ea99f0a554d2add809e
Patrikator17 <patkarpratik42>
2024-07-15 14:56:40 +0530

    fetch trending products fom wix

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,109 +1,59 @@
+import { myWixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
 import Image from "next/image"
 import Link from "next/link"
+import DOMPurify from "isomorphic-dompurify";
 
-const ProductList = () => {
+const PRODUCTs_PER_PAGE = 20
+
+const ProductList = async({categoryId, limit} : {categoryId:string; limit?:number}) => {
+
+    
+
+    const wixClient = await myWixClientServer()
+
+    const res = await wixClient.products.queryProducts().eq('collectionIds', categoryId).limit(limit || PRODUCTs_PER_PAGE).find()
+
+    // console.log(res.items[0].price?.formatted?.price);
+    
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-        <Link href='/test' className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"> 
-            <div className="relative w-full h-80">
-                <Image 
-                    src='https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                />
-                <Image 
-                    src='https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md"
-                />
-            </div>
-            <div className="flex justify-between">
-                <span className="font-medium">Product Name</span>
-                <span className="font-semibold">$49</span>
-            </div>
-            <div className="text-sm text-gray-500">
-                Product description
-            </div>
-            <button className="rounded-2xl ring-1 ring-cartRed text-cartRed w-max py-2 px-4 text-xs hover:bg-cartRed hover:text-white">Add to Cart</button>
-        </Link>
+        {res.items.map((product:products.Product) => (
 
-        <Link href='/test' className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"> 
+            <Link href={"/"+product.slug} className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"> 
             <div className="relative w-full h-80">
                 <Image 
-                    src='https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                    // src='https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                    src={product.media?.mainMedia?.image?.url || '/product.png'} 
                     alt="" 
                     fill sizes="25vw"
+                    key={product._id}
                     className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
                 />
-                <Image 
-                    src='https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md"
-                />
+                {product.media?.items && (
+                    <Image 
+                        // src='https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                        src={product.media?.items[1]?.image?.url || '/product.png'} 
+                        alt="" 
+                        fill sizes="25vw"
+                        className="absolute object-cover rounded-md"
+                    />
+                )}
+                
             </div>
             <div className="flex justify-between">
-                <span className="font-medium">Product Name</span>
-                <span className="font-semibold">$49</span>
+                <span className="font-medium">{product.name}</span>
+                <span className="font-semibold">{product.price?.formatted?.price}</span>
             </div>
-            <div className="text-sm text-gray-500">
-                Product description
+            <div className="text-sm text-gray-500" dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(product.additionalInfoSections?.find(
+                    (section: any) => section.title === 'shortDesc'
+                )?.description || "")}}>
+                
             </div>
             <button className="rounded-2xl ring-1 ring-cartRed text-cartRed w-max py-2 px-4 text-xs hover:bg-cartRed hover:text-white">Add to Cart</button>
         </Link>
-
-        <Link href='/test' className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"> 
-            <div className="relative w-full h-80">
-                <Image 
-                    src='https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                />
-                <Image 
-                    src='https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md"
-                />
-            </div>
-            <div className="flex justify-between">
-                <span className="font-medium">Product Name</span>
-                <span className="font-semibold">$49</span>
-            </div>
-            <div className="text-sm text-gray-500">
-                Product description
-            </div>
-            <button className="rounded-2xl ring-1 ring-cartRed text-cartRed w-max py-2 px-4 text-xs hover:bg-cartRed hover:text-white">Add to Cart</button>
-        </Link>
-
-        <Link href='/test' className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"> 
-            <div className="relative w-full h-80">
-                <Image 
-                    src='https://images.pexels.com/photos/4202325/pexels-photo-4202325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                />
-                <Image 
-                    src='https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt="" 
-                    fill sizes="25vw"
-                    className="absolute object-cover rounded-md"
-                />
-            </div>
-            <div className="flex justify-between">
-                <span className="font-medium">Product Name</span>
-                <span className="font-semibold">$49</span>
-            </div>
-            <div className="text-sm text-gray-500">
-                Product description
-            </div>
-            <button className="rounded-2xl ring-1 ring-cartRed text-cartRed w-max py-2 px-4 text-xs hover:bg-cartRed hover:text-white">Add to Cart</button>
-        </Link>
+        ))}
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,48 @@
 // "use client"
 
-// import { useContext, useEffect } from "react"
-// import CategoryList from "./components/CategoryList"
-// import ProductList from "./components/ProductList"
-// import Slider from "./components/Slider"
-// import { WixClientContext } from "./context/wixContext"
-
-// const HomePage = () => {
-
-//   const wixClient = useContext(WixClientContext)
-
-//   useEffect(() => {
-//     const getProducts = async() => {
-//       const res = await wixClient.products.queryProducts().find();
-//       console.log(res)
-//     }
-
-//     getProducts()
-//   }, [wixClient])
-  
-
-
-
-//   return (
-//     <div className=''>
-//       <Slider />
-//       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-//         <h1 className="text-2xl">Trending Products</h1>
-//         <ProductList />
-//       </div>
-//       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-//         <h1 className="text-2xl mb-12">Categories</h1>
-//         <CategoryList />
-//       </div>
-//       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-//         <h1 className="text-2xl">Just In</h1>
-//         <ProductList />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default HomePage
-
-"use client"
-
-import { useContext, useEffect } from "react"
+import { Suspense, useContext, useEffect } from "react"
 import CategoryList from "./components/CategoryList"
 import ProductList from "./components/ProductList"
 import Slider from "./components/Slider"
 import { WixClientContext } from "./context/wixContext"
+import { useWixClient } from "@/hooks/useWixClient"
+import { myWixClientServer } from "@/lib/wixClientServer"
 
-const HomePage = () => {
-  const wixClient = useContext(WixClientContext)
+// const HomePage = () => {
+//   const wixClient = useWixClient() // custom hook
 
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        if (!wixClient || !wixClient.products) {
-          throw new Error("Wix client or products module is not initialized correctly")
-        }
+//   useEffect(() => {
+//     const getProducts = async () => {
+//       try {
+//         if (!wixClient || !wixClient.products) {
+//           throw new Error("Wix client or products module is not initialized correctly")
+//         }
         
-        const res = await wixClient.products.queryProducts().find()
-        console.log("Products:", res)
-      } catch (error) {
-        console.error("Error fetching products:", error)
-      }
-    }
+//         const res = await wixClient.products.queryProducts().find()
+//         console.log("Products:", res)
+//       } catch (error) {
+//         console.error("Error fetching products:", error)
+//       }
+//     }
 
-    getProducts()
-  }, [wixClient])
+//     getProducts()
+//   }, [wixClient])
+
+const HomePage = async() => {
+  // const wixClient = await myWixClientServer()
+
+  // const res = await wixClient.products.queryProducts().find()
+  // console.log(res);
+  
 
   return (
     <div>
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Trending Products</h1>
-        <ProductList />
+        <Suspense fallback={"loading"}>
+          <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} limit = {4}/>
+        </Suspense>
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl mb-12">Categories</h1>
@@ -84,7 +50,7 @@ const HomePage = () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Just In</h1>
-        <ProductList />
+        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} limit = {4}/>
       </div>
     </div>
   )
